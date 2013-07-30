528234f48a471614c019015d5b0fec27ba4e4122
Kyle Simpson <getify@gmail.com>
2013-07-30 11:08:35 -0500

    v0.0.5-a: clarifying a bunch of recognition rules around regex literals, added complete regex literal test

---
+++ tests/8.js
@@ -0,0 +1,31 @@
+/* test #8
+ * testing how literalizer works
+ */
+
+return /a/g;
+throw /b/g;
+delete /c/g;
+in /d/g;
+else /e/g;
+void /f/g;
+typeof /g/g;
+yield /h/g;
+(/i/g)
+[/j/g]
+{/k/g}
++/l/g;
+-/m/g;
+*/n/g;
+=/o/g;
+~/p/g;
+!/q/g;
+%/r/g;
+&/s/g;
+,/t/g;
+|/u/g;
+;/v/g;
+:/w/g;
+?/x/g;
+</y/g;
+>/z/g;
+0/ /0/g;
